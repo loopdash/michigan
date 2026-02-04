e64a7e295e6f03bc6c527629cde26fde5df65dcc
Gary Bunofsky <garybunofsky@mac.lan>
2026-02-04 15:44:37 -0500

    remove config

---
+++ config.js
@@ -0,0 +1,7 @@
+// Configuration file for Mapbox API
+// This file is gitignored - add your own Mapbox token here
+// Get your token from: https://account.mapbox.com/access-tokens/
+
+window.MAPBOX_CONFIG = {
+    accessToken: 'pk.eyJ1IjoiZ2FyeWJ1bm9mc2t5IiwiYSI6ImNpc2h4b3MxbzAwMDAyb29jcHNuMmZmMGMifQ.NzIPO--_SRvVjW3M9WCtkw'
+};
